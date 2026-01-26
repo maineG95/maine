b86ad0cc0579512efc2f1d2f9a872a482b4e68f3
Sharmaine <sharmaine@skytech.ph>
2026-01-26 11:26:30 +0800

    Added About me redirection and resume

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../../core/components/services/local-storage-service';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ModalComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  isLoggedIn: boolean = false;
+  isModalVisible = false;
+  currentView: 'login' | 'signup' = 'login'; 
+
+ constructor(
+    private storage: LocalStorageService,
+     private router: Router
+  ) {}
+  
+   ngOnInit(): void {
+    const loggedIn = this.storage.get('isLoggedIn');
+    if (loggedIn === 'true') {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
+  }
+
+  openModal() {
+    this.isModalVisible = true;
+    this.currentView = 'login'; 
+  }
+
+  checkLoginAndNavigate(event: MouseEvent, route: string) {
+    event.preventDefault(); // Prevent default navigation
+    
+    if (this.isLoggedIn) {
+      this.router.navigate(['/me']);
+    } else {
+      this.router.navigate(['']);
+      this.openModal();
+    }
+  }
+
+  closeModalFromChild() {
+    this.isModalVisible = false;
+  }
+
+   openSignUp() {
+    this.currentView = 'signup';
+  }
 
 }
